177b7cc9f5d9e685068e75c16d064c4650988568
Jorge Roldan <jorge.roldan@gmail.com>
2025-05-16 15:24:44 +0200

    Working with stacks

---
+++ 11-data-structures-and-algorithms-in-python.py/src/1-work-with-linked-lists-and-stacks-and-understand-big-o-notation/3.3-using-pythons-lifoqueue.py
@@ -0,0 +1,11 @@
+# Import the module to work with Python's LifoQueue
+import queue
+
+# Create an infinite LifoQueue
+my_book_stack = queue.LifoQueue(maxsize=0)
+
+# Add an element to the stack
+my_book_stack.put("Don Quixote")
+
+# Remove an element from the stack
+my_book_stack.get()
