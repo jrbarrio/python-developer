50200a6b0fe6a25acd0f80eae727008d09a4d645
Jorge Roldan <jorge.roldan@gmail.com>
2025-05-16 15:13:12 +0200

    Understanding Big O Notation

---
+++ 11-data-structures-and-algorithms-in-python.py/src/1-work-with-linked-lists-and-stacks-and-understand-big-o-notation/2.2-practicing-with-big-o-notation.py
@@ -0,0 +1,9 @@
+colors = ['green', 'yellow', 'blue', 'pink']
+
+def linear(colors):
+  # Iterate the elements of the list
+  for color in colors:
+    # Print the current element of the list
+    print(color)	
+
+linear(colors)
