534805d3c42330024832b2c8b46cedba8bd34d65
Jorge Roldan <jorge.roldan@gmail.com>
2025-07-17 12:51:41 +0200

    Efficiently combining, counting, and itrating

---
+++ 01-writing-efficient-python-code/src/3-gaining-efficiencies/1.2-counting-pokemon-from-a-sample.py
@@ -0,0 +1,20 @@
+from collections import Counter
+
+names = ['Abomasnow', 'Abra', 'Absol', 'Accelgor', 'Aerodactyl']
+primary_types = ['Grass', 'Psychic', 'Dark', 'Bug', 'Rock']
+generations = [1, 1, 1, 5, 3]
+
+# Collect the count of primary types
+type_count = Counter(primary_types)
+print(type_count, '\n')
+
+# Collect the count of generations
+gen_count = Counter(generations)
+print(gen_count, '\n')
+
+# Use list comprehension to get each Pokémon's starting letter
+starting_letters = [name[0] for name in names]
+
+# Collect the count of Pokémon for each starting_letter
+starting_letters_count = Counter(starting_letters)
+print(starting_letters_count)
