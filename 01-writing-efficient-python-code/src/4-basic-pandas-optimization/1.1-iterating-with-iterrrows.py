c9d050706e6115bafa367c891e138ef84ec6d11f
Jorger Roldan <jorge.roldan@gmail.com>
2025-07-20 11:56:59 +0200

    Intro to pandas DataFrame iteration

---
+++ 01-writing-efficient-python-code/src/4-basic-pandas-optimization/1.1-iterating-with-iterrrows.py
@@ -0,0 +1,10 @@
+# Iterate over pit_df and print each index variable, row, and row type
+for i,row in pit_df.iterrows():
+    print(i)
+    print(row)
+    print(type(row))
+
+# Use one variable instead of two to store the result of .iterrows()
+for row_tuple in pit_df.iterrows():
+    print(row_tuple)
+    print(type(row_tuple))
