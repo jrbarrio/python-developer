ea497b082744147800402fa8d824ca874eb24c54
Jorger Roldan <jorge.roldan@gmail.com>
2025-07-21 09:52:05 +0200

    Another iterator method: .itertuples()

---
+++ 01-writing-efficient-python-code/src/4-basic-pandas-optimization/2.1-iterating-with-itertuples.py
@@ -0,0 +1,9 @@
+# Loop over the DataFrame and print each row's Index, Year and Wins (W)
+for row in rangers_df.itertuples():
+  i = row.Index
+  year = row.Year
+  wins = row.W
+  
+  # Check if rangers made Playoffs (1 means yes; 0 means no)
+  if row.Playoffs == 1:
+    print(i, year, wins)
