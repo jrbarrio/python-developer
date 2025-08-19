652eed53778493e7b744d8a9336533beff87f88a
Jorge Roldán <jorge.roldan@gmail.com>
2025-08-19 11:57:48 +0200

    Structuring imports

---
+++ 05-developing-python-packages/src/1-from-loose-code-to-local-packages/example-script.py
@@ -0,0 +1,17 @@
+from impyrial.length.api import convert_unit
+
+# Make sure unit checking is working by trying examples
+print('The following line should run:')
+result1 = convert_unit(10, 'in', 'yd')
+print(result1)
+
+print('The following line should cause an error:')
+result2 = convert_unit(10, 'lb', 'yd')
+print(result2)
+
+#############################################################3
+
+import impyrial
+
+result = impyrial.length.convert_unit(10, 'in', 'yd')
+print(result)
--- 05-developing-python-packages/src/1-from-loose-code-to-local-packages/impyrial/lenth/__init__.py
+++ 05-developing-python-packages/src/1-from-loose-code-to-local-packages/impyrial/lenth/__init__.py
@@ -4,3 +4,8 @@ impyrial
 A package for converting between imperial 
 measurements of length and weight.
 """
+
+# This is the __init__.py file for the impyrial/length subpackage
+
+# Import the convert_unit function from the api.py module
+from .api import convert_unit
