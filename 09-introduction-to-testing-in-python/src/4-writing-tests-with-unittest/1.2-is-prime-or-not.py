7883a394f9f5120572fa1a24020eadab01983158
Jorge Roldan <jorge.roldan@gmail.com>
2025-09-04 11:09:36 +0200

    Meeting the Unittest

---
+++ 09-introduction-to-testing-in-python/src/4-writing-tests-with-unittest/1.2-is-prime-or-not.py
@@ -0,0 +1,23 @@
+import unittest
+import math
+
+def is_prime(num):
+    if num == 1: return False
+    up_limit = int(math.sqrt(num)) + 1
+    for i in range(2, up_limit):
+        if num % i == 0:
+            return False
+    return True
+
+class TestSuite(unittest.TestCase):
+    def test_is_prime(self):
+        # Check that 17 is prime
+        self.assertTrue(is_prime(17))
+
+    def test_is_prime(self):
+        # Check that 6 is not prime
+        self.assertFalse(is_prime(6))
+
+    def test_is_prime(self):
+        # Check that 1 is not prime
+        self.assertFalse(is_prime(1))
